ab917e8c3cee0b553c9e61576f09164c8584179a
Jiing H Kueh <jhkueh@gmail.com>
2017-08-14 23:24:22 +1000

    Added superclass Entity

--- js/app.js
+++ js/app.js
@@ -1,5 +1,22 @@
+var Entity = function() {
+    this.x = -250;
+    this.y = 50;
+    
+    this.sprite = '';
+    
+    this.stepX = 100;
+    this.stepY = 80;
+}
+
+// Draw the Entity on the screen
+Entity.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(yCol, speed) {
+    Entity.call(this);
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
@@ -7,6 +24,9 @@ var Enemy = function() {
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
 };
+
+Enemy.prototype = Object.create(Entity.prototype);
+Enemy.prototype.constructor = Enemy;
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -16,33 +36,26 @@ Enemy.prototype.update = function(dt) {
     // all computers.
 };
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 var Player = function() {
-  this.sprite = 'images/char-boy.png';
+    Entity.call(this);
+    
+    this.sprite = 'images/char-boy.png';
   
-  this.x = 200;
-  this.y = 370;
-  
-  this.stepX = 100;
-  this.stepY = 80;
-  
+    this.x = 200;
+    this.y = 370;
+    this.defaultX = this.x;
+    this.defaultY = this.y;
 };
+
+Player.prototype = Object.create(Entity.prototype);
+Player.prototype.constructor = Player;
 
 Player.prototype.update = function() {
 
 };
-
-Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
 
 Player.prototype.handleInput = function(e) {
     switch (e) {
